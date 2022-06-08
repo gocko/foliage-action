8eaca8c6ac3080c726c742256a0a2744fe7ff634
Esteban Lorenzano <estebanlm@netc.eu>
2022-06-08 17:08:39 +0200

    and another

--- foliage.sh
+++ foliage.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-ls -l /home/runner/work/
-
-ls -l /home/runner/work/pharo-site
-
-ls -l /home/runner/work/pharo-site/pharo-site/
-
+set
 
 exec /opt/pharo/pharo /home/foliage/foliage.image --no-default-preferences clap foliage $@
