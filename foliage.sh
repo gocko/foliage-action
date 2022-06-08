707569be78b1f5d9edef31f82831d44da86d1c99
Esteban Lorenzano <estebanlm@netc.eu>
2022-06-08 15:54:33 +0200

    try to get colons out

--- foliage.sh
+++ foliage.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-pwd
-ls -la
-
-exec /opt/pharo/pharo /home/foliage/foliage.image --no-default-preferences clap foliage "$@"
+exec /opt/pharo/pharo /home/foliage/foliage.image --no-default-preferences clap foliage $@
