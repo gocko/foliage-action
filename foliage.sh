704d208b17ec4365aad3be817b9cf36439a15eb1
Esteban Lorenzano <estebanlm@netc.eu>
2022-06-09 11:26:13 +0200

    now we know where is executed, updating defaults

--- foliage.sh
+++ foliage.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-pwd
-
-ls -l 
-
-set 
-
-
-#exec /opt/pharo/pharo /opt/foliage/foliage.image --no-default-preferences clap foliage $@
+exec /opt/pharo/pharo /opt/foliage/foliage.image --no-default-preferences clap foliage $@
